578efb8823473ff04e325955d6fbbde00e893736
Joseph Margaryan <joe.2010@live.dk>
2025-11-03 13:42:51 +0100

    Add .gitignore

--- medlora/constants.py
+++ medlora/constants.py
@@ -4,8 +4,6 @@ import os
 from pathlib import Path
 from typing import Tuple, Sequence
 
-
-# full list (kept as list because it's used for argparse choices / ordering)
 MSD_TASKS: Sequence[str] = [
     "Task01_BrainTumour",
     "Task02_Heart",
@@ -19,7 +17,6 @@ MSD_TASKS: Sequence[str] = [
     "Task10_Colon",
 ]
 
-# sets used only for membership tests; make them immutable to avoid accidental mutation
 CT_TASKS = frozenset(
     [
         "Task03_Liver",
@@ -42,15 +39,11 @@ MRI_TASKS = frozenset(
 DEFAULT_VAL_FRAC = 0.2
 DEFAULT_ROI: Tuple[int, int, int] = (96, 96, 96)
 
-# SSL pre-trained weights (used by models.ensure_ssl / load_ct_ssl_encoder)
 SSL_URL = (
     "https://github.com/Project-MONAI/MONAI-extra-test-data/releases/download/0.8.1/"
     "ssl_pretrained_weights.pth"
 )
 
-# Default path for the downloaded SSL weights.
-# We should allow users to override this via the MEDLORA_SSL_PATH environment variable.
-# But for colab usage this might suffice?
 SSL_PATH = os.environ.get(
     "MEDLORA_SSL_PATH", str(Path.home() / ".medlora" / "ssl_pretrained_weights.pth")
 )
